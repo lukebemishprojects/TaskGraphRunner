d9889269be8970bd9d465b87342941622f76676a
Luke Bemish <lukebemish@lukebemish.dev>
2025-01-05 13:08:52 -0600

    Make memory detection with oshi more safe to errors

--- src/model/java/dev/lukebemish/taskgraphrunner/model/conversion/SystemSpecsFinder.java
+++ src/model/java/dev/lukebemish/taskgraphrunner/model/conversion/SystemSpecsFinder.java
@@ -1,13 +1,20 @@
 package dev.lukebemish.taskgraphrunner.model.conversion;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import oshi.SystemInfo;
 
 class SystemSpecsFinder {
+    private static final Logger LOGGER = LoggerFactory.getLogger(SystemSpecsFinder.class);
+
     static long recommendedThreads() {
         return Runtime.getRuntime().availableProcessors() / 2;
     }
 
     static long recommendedMemory() {
+        if (TOTAL_MEMORY == -1) {
+            return GIGABYTE * 4;
+        }
         return Math.min(TOTAL_MEMORY / 5, GIGABYTE * 4);
     }
 
@@ -16,8 +23,14 @@ class SystemSpecsFinder {
     private static final long GIGABYTE = 1024 * 1024 * 1024;
 
     static {
-        var systemInfo = new SystemInfo();
-        var hardware = systemInfo.getHardware();
-        TOTAL_MEMORY = hardware.getMemory().getTotal();
+        long totalMemory = -1;
+        try {
+            var systemInfo = new SystemInfo();
+            var hardware = systemInfo.getHardware();
+            totalMemory = hardware.getMemory().getTotal();
+        } catch (Throwable e) {
+            LOGGER.warn("Failed to get system specs; memory allocated to tasks may not be optimal", e);
+        }
+        TOTAL_MEMORY = totalMemory;
     }
 }
--- src/model/java/module-info.java
+++ src/model/java/module-info.java
@@ -3,6 +3,7 @@ module dev.lukebemish.taskgraphrunner.model {
     requires static org.jspecify;
     requires dev.lukebemish.forkedtaskexecutor;
     requires com.github.oshi;
+    requires org.slf4j;
 
     opens dev.lukebemish.taskgraphrunner.model.conversion to com.google.gson;
     opens dev.lukebemish.taskgraphrunner.model to com.google.gson;
